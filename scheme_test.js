8c46eac3cd97aa88d127e6fb712f89e35f4c93f1
Clark Kampfe <clark.kampfe@gmail.com>
2014-03-19 12:13:01 -0500

    reworked tests into expectations vs shoulds

--- scheme_test.js
+++ scheme_test.js
@@ -1,47 +1,50 @@
-var should = require('should');
-var S = require('./scheme')();
+var expect = require('chai').expect
+var evalScheme = require('./scheme')();
 
-describe('#schemeEval()', function () {
-
+describe('#eval()', function () {
   describe('CHALLENGE 1: BASICS', function () {
     it('should evaluate numbers', function () {
-      S.schemeEval('(1)').should.eql(1);
+      expect(evalScheme('(1)')).to.equal(1);
     });
 
     it('should evaluate booleans: true', function () {
-      S.schemeEval('#t').should.be.true
+      expect(evalScheme('#t')).to.be.true;
     });
 
     it('should evaluate booleans: false', function () {
-      S.schemeEval('#f').should.be.false
+      expect(evalScheme('#f')).to.be.false;
     });
   });
 
   describe('CHALLENGE 2: BASIC MATH', function () {
     it('should evaluate addition', function () {
-      S.schemeEval('(+ 1 2)').should.eql(3);
+      expect(evalScheme('(+ 1 2)')).to.equal(3);
     });
 
     it('should evaluate multiplication', function () {
-      S.schemeEval('(* 4 13 2)').should.eql(104);
+      expect(evalScheme('(* 4 13 2)')).to.equal(104);
     });
 
     it('should evaluate division', function () {
-      S.schemeEval('(/ 16 4)').should.eql(4);
+      expect(evalScheme('(/ 16 4 2)')).to.equal(2);
     });
 
     it('should evaluate subtraction', function () {
-      S.schemeEval('(- 25 8 4)').should.eql(13);
+      expect(evalScheme('(- 25 8 4)')).to.equal(13);
     });
   });
 
   describe('CHALLENGE 3: NESTING', function () {
     it('evaluates nested arithmetic', function () {
-      S.schemeEval('(+ 1 (* 2 3))').should.eql(7);
+      expect(evalScheme('(+ 1 (* 2 3))')).to.equal(7);
     });
 
     it('evaluates arbitrarily nested arithmetic', function () {
-      S.schemeEval('(+ 1 (* 2 (- 9 3)))').should.eql(13);
+      expect(evalScheme('(+ 1 (* 2 (- 9 3)))')).to.equal(13);
+      expect(evalScheme('(- 11 (* 3 (/ 12 3)))')).to.equal(-1);
+      expect(evalScheme('(- 78 (* 5 (- 15 (/ 20 4))))')).to.equal(28);
+    });
+  });
     });
   });
 });
