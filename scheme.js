1e1c3176582e10da2220dc254ad154813a878371
Clark Kampfe <clark.kampfe@gmail.com>
2014-03-19 12:22:29 -0500

    + - * / nesting and tests

--- scheme.js
+++ scheme.js
@@ -1,103 +1,156 @@
 module.exports = function () {
-  return {
-    schemeEval: function (exp) {
+  if (!Array.prototype.flatten) {
+    Array.prototype.flatten = function () {
+      var result = [];
+      return result.concat.apply(result, this);
+    }
+  }
 
+  // split off parens
+  if (!Array.prototype.splitParens) {
+   Array.prototype.splitParens = function (re) {
+      return this.map(function (el) {
+        return el.split(re);
+      }).flatten();
+    }
+  }
 
-      return loop_run(exp, []);
+  function tokenize (exp) {
+    var tokens = exp.split(' ');
+    var leftParen = /(\()/;
+    var rightParen = /(\))/;
 
-      function loop_run(exp, stack) {
-        // console.log(exp, stack);
-        // REMOVE OUTER PARENS
+    // split parens
+    tokens = tokens.splitParens(leftParen).splitParens(rightParen);
 
-        // RETURN A SINGLE NUMBER, LIKE (3341)
-        if (Number(exp)) {
-          return Number(exp);
-        }
+    // remove spaces
+    tokens = tokens.filter(function (token) {
+      return token !== '';
+    });
 
-        // RETURN A BOOLEAN
-        switch (exp) {
-          case '#t':
-            return true;
+    return tokens;
+  }
+
+  function parse (tokens) {
+    var token = tokens.shift();
+
+    if (token === '(') {
+      var list = [];
+      while (tokens[0] !== ')') {
+        list.push(parse(tokens));
+      }
+
+    return list;
+    } else if (token === ')') {
+      throw "parenError";
+    } else {
+      return atom(token);
+    }
+  }
+
+  function atom (token) {
+    if (Number(token) == token) {
+      return Number(token);
+    }
+
+    return token;
+  }
+
+  // MAIN
+  return function eval(exp) {
+    var stack = parse(tokenize(exp));
+
+    if (typeof stack === 'string') {
+      stack = Array(stack);
+    }
+
+    // eval loop
+    return (function rec (stack) {
+
+      // truthy/numbery base cases
+      if (stack.length === 1) {
+        var result = stack[0];
+        switch (typeof result) {
+          case 'string':
+            if (result === '#t') {
+              return true;
+            } else if (result === '#f') {
+              return false;
+            } else {
+              return result;
+            }
             break;
-          case '#f':
-            return false;
+          case 'number':
+            return result;
             break;
-        }
-
-        // try to get the innermost list
-        try {
-          var innerMostRe = /^\(.*(\(.*\))\)$/,
-            innerMostExp = exp.match(innerMostRe)[1],
-            nextExp = prep(innerMostExp),
-            value = inject(nextExp.exp, nextExp.operator);
-
-          console.dir(exp);
-          var newExp = exp.replace(innerMostExp, value.toString());
-
-          console.dir(newExp);
-          return loop_run(newExp, stack);
-
-        // else...
-        } catch(error) {
-          console.dir(error);
-          // if it's a single list item, return it
-          if (exp.match(/^\(\S+\)$/)) {
-            return trimParens(exp);
-          }
-
-          var injectables = prep(exp);
-
-          return loop_run(inject(injectables.exp, injectables.operator), stack);
-        }
-
-        // HELPERS
-        function prep (exp) {
-          exp = trimParens(exp).split(' ');
-          var operator = exp.shift();
-          exp = exp.map(function (item) {
-            return Number(item);
-          });
-
-          return {
-            exp: exp,
-            operator: operator
-          }
-        }
-
-        function trimParens (exp) {
-          return exp.slice(1, -1);
-        }
-
-        // there has got to be a way to do this more elegantly
-        function inject (list, operator) {
-          switch (operator) {
-            case '+':
-              return list.reduce(function (x, y) {
-                return x + y;
-              });
-
-              break;
-            case '-':
-              return list.reduce(function (x, y) {
-                return x - y;
-              });
-
-              break;
-            case '*':
-              return list.reduce(function (x, y) {
-                return x * y;
-              });
-
-              break;
-            case '/':
-              return list.reduce(function (x, y) {
-                return x / y;
-              });
-
-              break;
-          }
+          default:
+            return result;
+            break;
         }
       }
-    }
+
+      // current element
+      var current = stack.shift();
+
+      if (Array.isArray(current)) {
+        // recurse
+        return rec(current);
+
+        // MATH
+      } else if (current === '+') {
+        var result = mathEval(addRed);
+
+        if (stack.length === 0) {
+          return result;
+        }
+
+        return result + rec(stack.shift());
+
+      } else if (current === '-') {
+        var result = mathEval(subRed);
+        if (stack.length === 0) {
+          return result;
+        }
+
+        return result - rec(stack.shift());
+
+      } else if (current === '*') {
+        var result = mathEval(multRed);
+        if (stack.length === 0) {
+          return result;
+        }
+
+        return result * rec(stack.shift());
+
+      } else if (current === '/') {
+        var result = mathEval(divRed);
+
+        if (stack.length === 0) {
+          return result;
+        }
+
+        return result / rec(stack.shift());
+
+      } else if (typeof current === 'number') {
+        return rec(stack);
+        return current;
+      }
+
+      // create math substack, eval what can be eval'd
+      function mathEval (opFn) {
+        var mathStack = [];
+        while (!Array.isArray(stack[0]) && stack.length > 0) {
+          mathStack.push(stack.shift());
+        }
+
+        return mathStack.reduce(opFn);
+      }
+
+      // mathematical operator callbacks for reduce
+      function addRed (a, b) { return a + b; }
+      function subRed (a, b) { return a - b; }
+      function multRed (a, b) { return a * b; }
+      function divRed (a, b) { return a / b; }
+    })(stack);
   }
 }
